e9d2c627b3cf626b47f09619d7824e04f2e60337
Aleksandra1311 <77723241+Aleksandra1311@users.noreply.github.com>
2021-08-13 15:38:34 +0300

    fix
    fix

--- src/js/model/model.js
+++ src/js/model/model.js
@@ -2,11 +2,10 @@
 import { PHOTOS_URL } from '../config';
 import { photosArr } from '../model/collection';
 
-export function getPhotos(albumId) { // albumId это идентификатр 1 альбома
+export function getPhotos(albumId) { 
     return fetch(PHOTOS_URL + albumId)
         .then((resp) => resp.json())
         .then((data) => photosArr = data)
-        // .then(renderPhotos); //в этой ф-ии нужно убрать renderAlbums, он должен вызываться из controller
 }
 
 
