cdd72e6a5705b9e814391bd1b544a698c0a07938
Aleksandra1311 <77723241+Aleksandra1311@users.noreply.github.com>
2021-08-13 15:39:43 +0300

    fix
    fix

--- src/js/view/albumsView.js
+++ src/js/view/albumsView.js
@@ -1,25 +1,24 @@
 import { getPhotos } from '../model/model';
-// import { getPhotos } from '../model/collection';
 
 const ALBUM_ITEM_CLASS = 'album-item';
  
-const albumsEl = document.querySelector('#albums');//выборка елемент для рендера
+const albumsEl = document.querySelector('#albums');
 
 albumsEl.addEventListener('click', onAlbumsClick);
 
-function onAlbumsClick(e) { //делигирование, навешиваем 1 обраб на всё
-    if (e.target.classList.contains(ALBUM_ITEM_CLASS)) { //провер, клик произошёл по эл альбома, есть там такой class?
-        getPhotos(e.target.dataset.id) // если class есть то и e.target.dataset.id тоже есть , берём его значение(будет id альбома) и подставляем в getPhotos
+function onAlbumsClick(e) { 
+    if (e.target.classList.contains(ALBUM_ITEM_CLASS)) { 
+        getPhotos(e.target.dataset.id) 
     }
 }
 
-export function renderAlbums(data) { //взяли тот массив который прилетит c getAlbums()
+export function renderAlbums(data) { 
     albumsEl.innerHTML = data
-        .map(album => generateAlbumHtml(album)) //превратили в строку этот массив и превращаю его в html
+        .map(album => generateAlbumHtml(album)) 
         .join('\n');
 }
 
- function  generateAlbumHtml(album){ //превращение в html
+ function  generateAlbumHtml(album){
     return `<div class="album-item" data-id="${album.id}">${album.title}</div>`
         .replace('{{id}}', album.id)
         .replace('{{title}}', album.title);
