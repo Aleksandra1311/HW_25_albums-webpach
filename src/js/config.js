1099e2afc4648f83e6cebb2208e679f7425fd9ef
Aleksandra1311 <77723241+Aleksandra1311@users.noreply.github.com>
2021-08-13 15:40:38 +0300

    fix

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,2 @@
 export const ALBUMS_URL = 'https://jsonplaceholder.typicode.com/albums'; //список всех альбомой
 export const PHOTOS_URL = 'https://jsonplaceholder.typicode.com/photos?albumId=';
-
-
-// export const ALBUMS_URL = 'https://jsonplaceholder.typicode.com/albums'; //список всех альбомой
-// export const PHOTOS_URL = 'https://jsonplaceholder.typicode.com/photos?albumId={{id}}';//список всех фотографий альбома
